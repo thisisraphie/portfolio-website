b77a7598ec6b6249efa1ed155282e426d00c4e1b
thisisraphie <raphielevita@gmail.com>
2026-04-09 20:43:21 +0800

    refactor: switched to angular

---
+++ portfolio/src/app/pages/certificates/certificates.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-certificates',
+  imports: [],
+  templateUrl: './certificates.html',
+  styleUrl: './certificates.css',
+})
+export class Certificates {}
